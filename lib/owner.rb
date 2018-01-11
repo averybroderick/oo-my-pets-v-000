d08cd820829ecba174ae0a9918f50f198e8f5ad7
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-11 17:36:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -76,7 +76,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{@pets[:fish].length} fish"
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
 end
