9c5473a6e9ca210071712c2c58ff0c8e7a6a1a28
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-11 17:05:34 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,38 @@
 class Owner
-  # code goes here
+
+  attr_accessor :owner, :pets
+  attr_reader :species
+
+  @@all = []
+
+  #CLASS METHODS
+
+  def initialize(species)
+    @species = species
+    @pets = {
+      fishes: [],
+      cats: [],
+      dogs: []
+    }
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  #INSTANCE METHODS
+
+  def species
+    @species
+  end
+
 end
